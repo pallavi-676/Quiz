1c00919ed2d12e587d331aac662bbe398d1133db
pallavi-676 <pallvaisarovar1099@gmail.com>
2025-12-16 16:13:18 +0530

    Add python file

---
+++ Quiz.py
@@ -0,0 +1,40 @@
+# questions=  [
+#     {"question": "What is capital of India?",
+#      "options" :["A. Mumbai", "B. Jaipur", "C. Delhi", "D. Goa"],
+#      "Answer": "C"},
+#      {"question": "Who won women's tata wpl in 2024?",
+#       "options":["A. Rcb", "B. Mi", "C. Dc", "D. Gl"],
+#       "Answer": "A"},
+#       {"question": "Which colours is lotus?", 
+#        "options": ["A. Pink", "B. Red", "C. Black", "D. all of the above"],
+#        "Answer": "A"},
+#        {"question": "Which planet is known as the Red Planet?",
+#         "options": ["A. Venus", "B. Mars", "C. Jupiter", "D. Saturn"],
+#         "Answer": "B"},
+#         {"question": "Which country is known as the Land of the Rising Sun?",
+#         "options": ["A. China", "B. South Korea", "C. Japan", "D. Thailand"],
+#         "Answer": "C"},
+#         {"question": "Who is known as the father of computers?",
+#         "options": ["A. Albert Einstein", "B. Isaac Newton", "C. Charles Babbage", "D. Thomas Edison"],
+#         "Answer": "C"}
+# ]
+# prizes= [100,200,500,1000,2000,4000]
+# total_prize= 0
+
+
+# for i,q in enumerate(questions):
+#     print(f"\nQuestion {i+1}: {q['question']}")
+#     for option in q["options"]:
+#         print(option)
+#     answer= input("Enter your choice (A/B/C/D): ").strip().upper()
+
+#     if answer ==q['Answer']:
+#         print(f"Correct! You win inr {prizes[i]}.")
+#         total_prize += prizes[i]
+#     else:
+#         print(f"Wrong! The correct answer is: {q['Answer']}")
+
+# print(f"\nCongratulations!!! You are taking home inr {total_prize}. ")
+# print("Thank you for playing🙏🏻")
+
+
